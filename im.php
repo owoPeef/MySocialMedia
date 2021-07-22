3d11b90769f675b6e5e58957e6b574207918971a
owoPeef <zoomytfunny@gmail.com>
2021-07-22 21:29:35 +0300

    commit

--- im.php
+++ im.php
@@ -7,24 +7,24 @@ if (isset($_SESSION['user']['user_id']))
     {
         $sel = $_GET['sel'];
         $user_id = $_SESSION['user']['user_id'];
-        $data = mysqli_query($db, "SELECT * FROM `messages` WHERE (`from_user`='{$user_id}' OR `to_user`='{$user_id}') AND (`from_user`='{$sel}' OR `to_user`='{$sel}') ORDER BY `send_date` ASC LIMIT 10");
+        if ($sel == $user_id)
+        {
+            $data = mysqli_query($db, "SELECT * FROM `messages` WHERE `from_user`='{$user_id}' AND `to_user`='{$user_id}' ORDER BY `send_date` ASC LIMIT 10");
+        }
+        if ($sel != $user_id)
+        {
+            $data = mysqli_query($db, "SELECT * FROM `messages` WHERE (`from_user`='{$user_id}' OR `to_user`='{$user_id}') AND (`from_user`='{$sel}' OR `to_user`='{$sel}') ORDER BY `send_date` ASC LIMIT 10");
+        }
         while($row = mysqli_fetch_array($data))
         {
             $send_time = date('H:i:s', strtotime($row[4]));
-            if ($row[1] == $row[2] && $user_id == $row[1] && $user_id == $row[2])
+            if ($row[1] == $user_id)
             {
-                echo "<h2>$row[3]</h2><h5>$send_time</h5>";
+                echo "<div class='left_message'><h2 class='left_offset'>$row[3]</h2><h5 class='left_date'>$send_time</h5></div>";
             }
-            elseif ($row[1] != $row[2])
+            if ($row[2] == $user_id && $row[1] != $row[2])
             {
-                if ($row[1] == $user_id || $row[2] == $user_id)
-                {
-                    echo "<h2>$row[3]</h2><h5>$send_time</h5>";
-                }
-                if ($row[1] == $sel || $row[2] == $sel)
-                {
-                    echo "<div class='right_message'><h2 class='right_offset'>$row[3]</h2><h5>$send_time</h5></div>";
-                }
+                echo "<div class='right_message'><h2 class='right_offset'>$row[3]</h2><h5 class='right_date'>$send_time</h5></div>";
             }
         }
     }
